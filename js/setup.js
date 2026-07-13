0e226b1a351aade2d93d6ccc95a8fa7233ee1d8d
sagar joshi <sagarjo637@gmail.com>
2026-07-13 17:54:21 +0545

    Implement room member management

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,6 @@
 //storing members temporary
 let members = [];
+
 // selecting element
 const roomNameInput = document.getElementById("roomName");
 const memberInput = document.getElementById("memberInput");
@@ -8,16 +9,30 @@ const memberList = document.getElementById("memberList");
 const budgetInput = document.getElementById("budget");
 const createRoomBtn = document.getElementById("createRoomBtn");
 
+console.log(memberList);
+
 //add member
 function addMember() {
   const readMemberInput = memberInput.value.trim();
+
   if (readMemberInput === "") {
     alert("please enter member");
     return;
   }
-  members.push(readMemberInput);
-  console.log(members);
+
+  preventDuplicate(readMemberInput);
+
   memberInput.value = "";
+  renderMembers();
+}
+
+//function to prevent duplicate name
+function preventDuplicate(name) {
+  if (members.includes(name)) {
+    alert(`${name} already exists!`);
+  } else {
+    members.push(name);
+  }
 }
 
 //button click
@@ -28,3 +43,31 @@ memberInput.addEventListener("keypress", (event) => {
     addMember();
   }
 });
+
+//render members
+function renderMembers() {
+  memberList.innerHTML = "";
+
+  members.forEach((member, idx) => {
+    const memberCard = document.createElement("div");
+    memberCard.className = "bg-slate-200 rounded-xl p-3 flex justify-between";
+
+    const span = document.createElement("span");
+    span.textContent = `👤 ${member} `;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "bg-blue-600 px-6 py-2 rounded text-white ";
+    removeBtn.innerHTML = "Remove";
+
+    removeBtn.addEventListener("click", () => {
+      members.splice(idx, 1);
+      renderMembers();
+    });
+
+    memberCard.appendChild(span);
+    memberCard.appendChild(removeBtn);
+    memberList.appendChild(memberCard);
+  });
+}
+
+renderMembers();
